6bc323009e8853c3e4f3feefe8ce9759d217f303
mapalmeira <matheus.palmeira.leite@gmail.com>
2026-09-06 15:06:47 -0300

    fix(ui): prefix and suffix whitespaces

--- backend/app/infrastructure/persistence/sqlite/databases.py
+++ backend/app/infrastructure/persistence/sqlite/databases.py
@@ -11,12 +11,12 @@ from app.infrastructure.persistence.sqlite.registry.unit_of_work import SqliteRe
 
 
 _DEFAULT_LEDGER_CURRENCIES = (
-    ("Real", "R$", None, 2, "unicode:R$", bytes.fromhex("FFD51A")),
-    ("Dollar", "$", None, 2, "unicode:$", bytes.fromhex("2E7D32")),
-    ("Euro", "€", None, 2, "unicode:€", bytes.fromhex("003399")),
-    ("Bitcoin", None, "BTC", 8, "unicode:₿", bytes.fromhex("F7931A")),
-    ("Iene", "¥", None, 0, "unicode:¥", bytes.fromhex("BC002D")),
-    ("Libra", "£", None, 2, "unicode:£", bytes.fromhex("5B2C6F")),
+    ("Real", "R$ ", None, 2, "unicode:R$", bytes.fromhex("FFD51A")),
+    ("Dollar", "$ ", None, 2, "unicode:$", bytes.fromhex("2E7D32")),
+    ("Euro", "€ ", None, 2, "unicode:€", bytes.fromhex("003399")),
+    ("Bitcoin", None, " BTC", 8, "unicode:₿", bytes.fromhex("F7931A")),
+    ("Iene", "¥ ", None, 0, "unicode:¥", bytes.fromhex("BC002D")),
+    ("Libra", "£ ", None, 2, "unicode:£", bytes.fromhex("5B2C6F")),
 )
 
 
